2e7f230afc82dbf7991ab962684b928e65d3a87e
Caua Rezende <cauagrezende@gmail.com>
2020-01-01 18:45:47 -0300

    ensure LoginRouter call AuthUseCase with correct params

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,6 +1,10 @@
 const HttpReponse = require('../helpers/http-response')
 
 module.exports = class LoginRouter {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
       return HttpReponse.serverError()
@@ -14,5 +18,7 @@ module.exports = class LoginRouter {
     if (!password) {
       return HttpReponse.badRequest('password')
     }
+
+    this.authUseCase.auth(email, password)
   }
 }
