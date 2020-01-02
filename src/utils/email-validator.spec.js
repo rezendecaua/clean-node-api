94e8a3824cf995d030b5492bb452b243cc27176e
Caua Rezende <cauagrezende@gmail.com>
2020-01-01 21:32:18 -0300

    ensure EmailValidator returns true if validator returns true

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,14 @@
+class EmailValidator {
+  isValid (email) {
+    return true
+  }
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator return true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+
+    expect(isEmailValid).toBe(true)
+  })
+})
