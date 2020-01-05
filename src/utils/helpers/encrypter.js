aa6678f169f2f221484f7da93c9319f1acb37c9a
Caua Rezende <cauagrezende@gmail.com>
2020-01-05 08:26:32 -0300

    ensure Encrypter throws if no params are provided

--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -1,10 +1,23 @@
 const bcrypt = require('bcrypt')
+const MissingParamError = require('../errors/missing-param-error')
 
 module.exports = class Encrypter {
   async compare (value, hash) {
+    this.validate(value, hash)
+
     this.value = value
     this.hash = hash
     const isValid = await bcrypt.compare(value, hash)
     return isValid
+  }
+
+  validate (value, hash) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+
+    if (!hash) {
+      throw new MissingParamError('hash')
+    }
   }
 }
