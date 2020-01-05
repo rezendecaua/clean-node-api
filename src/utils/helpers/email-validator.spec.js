0873e0c09336aea990dcfd10e4fb32ca92ee0ae0
Caua Rezende <cauagrezende@gmail.com>
2020-01-05 08:31:10 -0300

    ensure EmailValidator throws if no param is provided

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
 const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const MissingParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
   return new EmailValidator()
@@ -26,5 +27,10 @@ describe('Email Validator', () => {
     sut.isValid('any_email@email.com')
 
     expect(validator.email).toBe('any_email@email.com')
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const sut = makeSut()
+    expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
